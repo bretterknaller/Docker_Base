a89714dd344448685cee224d29fae4c4a7db187f
bretterknaller <sawmozart@hotmail.com>
2015-11-26 12:08:16 +0100

    add

---
+++ Setup.sh
@@ -0,0 +1,38 @@
+
+Setup () {
+COLUMNS=1
+PS3="Nummer angeben : "
+clear
+echo "(1) ADD Software"
+echo "(2) Remove Software"
+echo "(3) Box löschen"
+read -p "Bitte Nummer angeben:" status
+case "$status" in
+1) 	 
+	echo "(1) Pyload"
+	
+	read -p "Bitte Nummer angeben:" Software
+	
+Add () {
+
+case "$Software" in
+1) Pyload
+	cat ./set_pyload >> ./Dockerfile
+;;
+2) #enigma2-pli-nightly 
+;;
+3) #neutrino 
+;;
+4) #xbmc-nightly 
+;; 	
+esac
+cp -rf $USERDIR/patch_Git/* $HOMEDIR/Git/tdt 2>/dev/null
+echo "<<<<------------------Docker_Setup------------------>>>>"	
+echo "<<<<------------------------------------------------------>>>>"
+echo "(1) Setup"
+echo "<<<<------------------------------------------------------>>>>"
+echo "(2) Single Bauen"
+echo "(3) Multi Bauen"
+echo "(4) Flash erstellen"
+echo "(5) Git/NHD2 up/downgraden"
+echo "<<<<------------------------------------------------------>>>>"
