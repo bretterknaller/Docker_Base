5f7920f6c2e410aeba08b7ba46e57051a42f6ff8
br <br@Server.fritz.box>
2015-11-29 00:50:39 +0100

    Commit-Nachricht

--- Setup.sh
+++ Setup.sh
@@ -1,43 +1,45 @@
-
-Setup () {
-COLUMNS=1
-PS3="Nummer angeben : "
-clear
-echo "(1) ADD Software"
-echo "(2) Remove Software"
-echo "(3) Box löschen"
-read -p "Bitte Nummer angeben:" status
-case "$status" in
-1) 	 
-	echo "(1) Pyload"
-	
-	read -p "Bitte Nummer angeben:" Software
-}
-	
-Add () {
-
-case "$Software" in
-1) Pyload
-	cat ./set_pyload >> ./Dockerfile
-;;
-2) #enigma2-pli-nightly 
-;;
-3) #neutrino 
-;;
-4) #xbmc-nightly 
-;; 	
+Software () {
+# Demonstriert die select-Anweisung
+# aselect1
+select auswahl in \
+Pyload \
+Bla \
+exit
+do
+case $auswahl in 
+Pyload) cat ./set_pyload >> ./Dockerfile ;;
+Bla) echo "bla" ;;
+exit) exit ;;
 esac
+done
 }
-
-
-echo "<<<<------------------Docker_Setup------------------>>>>"	
+clear
+echo "<<<<------------------Docker_Gitbuilder------------------->>>>"
 echo "<<<<------------------------------------------------------>>>>"
-echo "(1) Setup"
+echo "(1) Software Angeben"
 echo "<<<<------------------------------------------------------>>>>"
 echo "(2) Single Bauen"
 echo "(3) Multi Bauen"
 echo "(4) Flash erstellen"
 echo "(5) Git/NHD2 up/downgraden"
 echo "<<<<------------------------------------------------------>>>>"
+echo "(6) TDT Setup"
+echo "(7) Git Klonen"
+echo "(8) Git säubern"
+echo "<<<<------------------------------------------------------>>>>"
+echo "(0) EXIT"
 
+read -p "Bitte Nummer angeben:" status
+
+case $status in
+        1) Software ;;
+        2) Single ;;
+        3) Multi ;;
+        4) Flash ;;
+        5) Up_Down ;;
+        6) TDT_Setup ;;
+        7) Git_Klonen ;;
+        8) ClearGit;;
+        *) exit ;;
+esac
 exit
